b41faaa3cef9eaeda1eac456a8139c69a339fffb
Hewang Ma <xzz14939@gmail.com>
2025-05-02 01:24:23 +0800

    debug vid -thumbs

--- utils/vids_thumb.py
+++ utils/vids_thumb.py
@@ -7,18 +7,10 @@ import sys
 import subprocess
 
 
-def rename_all(dir):
-    for i, file in enumerate(os.listdir(dir)):
-        print(file)
-        new_name = str(i) + file[-4:]
-        os.rename(dir + file, dir + new_name)
-        print(f"Renamed {file} to {new_name}")
-
-
 def vid_gen_thumb(vid_path):
-    '''
+    """
     给一个vid路径，在该路径下生成同名的thumb
-    '''
+    """
 
     def capture_frames(vid_path, num_frames):
         cap = cv2.VideoCapture(vid_path)
@@ -51,8 +43,7 @@ def vid_gen_thumb(vid_path):
         rows = (len(resized_frames) + columns - 1) // columns
         thumbnail_height = rows * thumb_height
         thumbnail_width = columns * thumb_width
-        thumbnail = np.zeros(
-            (thumbnail_height, thumbnail_width, 3), dtype=np.uint8)
+        thumbnail = np.zeros((thumbnail_height, thumbnail_width, 3), dtype=np.uint8)
         for i, frame in enumerate(resized_frames):
             row = i // columns
             col = i % columns
@@ -64,8 +55,11 @@ def vid_gen_thumb(vid_path):
 
         return thumbnail
 
-    filename_list = vid_path.split('.')
-    thumb_path = f"{filename_list[0]}.jpg"
+    filename_list = vid_path.split(".")
+    thumb_path = f"{filename_list[0]}_thumb.jpg"
+    if os.path.exists(thumb_path):
+        print(f"{thumb_path} exists")
+        return
     thumb_path = os.path.abspath(thumb_path)
     num_frames = 20
     frames, width, height = capture_frames(vid_path, num_frames)
@@ -78,112 +72,124 @@ def vid_gen_thumb(vid_path):
 
 
 vid_forms = [
-    'avi',
-    'AVI',
-    'mp4',
-    'MP4',
-    'mov',
-    'MOV',
-    'wmv',
-    'WMV',
-    'mkv',
-    'MKV',
-    'flv',
-    'FLV',
-    'webm',
-    'WEBM',
-    'mpeg',
-    'MPEG',
-    'mpg',
-    'MPG',
-    'm4v',
-    'M4V',
-    '3gp',
-    '3GP',
-    'asf',
-    'ASF',
-    'vob',
-    'VOB',
-    'ts',
-    'TS',
-    'm2ts',
-    'M2TS',
-    'divx',
-    'DIVX',
-    'rm',
-    'RM',
-    'rmvb',
-    'RMVB',
-    'ogv',
-    'OGV',
-    'mxf',
-    'MXF',
-    'mpv',
-    'MPV',
-    'hevc',
-    'HEVC',
+    "avi",
+    "AVI",
+    "mp4",
+    "MP4",
+    "mov",
+    "MOV",
+    "wmv",
+    "WMV",
+    "mkv",
+    "MKV",
+    "flv",
+    "FLV",
+    "webm",
+    "WEBM",
+    "mpeg",
+    "MPEG",
+    "mpg",
+    "MPG",
+    "m4v",
+    "M4V",
+    "3gp",
+    "3GP",
+    "asf",
+    "ASF",
+    "vob",
+    "VOB",
+    "ts",
+    "TS",
+    "m2ts",
+    "M2TS",
+    "divx",
+    "DIVX",
+    "rm",
+    "RM",
+    "rmvb",
+    "RMVB",
+    "ogv",
+    "OGV",
+    "mxf",
+    "MXF",
+    "mpv",
+    "MPV",
+    "hevc",
+    "HEVC",
 ]
 
 
+def convert_format(prefix, ext):
+    file_path = f"{prefix}.{ext}"
+    if ext.lower() == "mp4":
+        return file_path
+    converted_file_path = f"{prefix}_convert_from_{ext}.mp4"
+    if os.path.exists(converted_file_path):
+        return converted_file_path
+
+    print(f"Changing {file_path} to mp4")
+    result = subprocess.run(
+        [
+            "ffmpeg",
+            "-i",
+            file_path,
+            "-vcodec",
+            "libx264",
+            "-acodec",
+            "aac",
+            converted_file_path,
+        ],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+    )
+
+    if result.returncode == 0:
+        print(f"Successfully converted {file_path} to MP4, and reomve {file_path}")
+        os.remove(file_path)
+    else:
+        print(f"error during converting")
+    return converted_file_path
+
+
 def gen_thumbs_and_unify_to_mp4(dir):
-    '''
+    """
     将所有视频转换为mp4；
     并生成预览图
-    '''
+    """
     for root, _, files in os.walk(dir):
         for file in files:
 
             filepath = os.path.join(root, file)
-            if any(filepath.endswith(f'.{form}.jpg') for form in vid_forms):
+            if any(filepath.endswith(f".{form}.jpg") for form in vid_forms):
                 os.remove(filepath)
+                print("\n??\n")
                 continue
-            filename_list = filepath.split('.')
-            prefix, ext = '_'.join(
-                filename_list[:-1]).replace(' ', "_"), filename_list[-1]
+            filename_list = filepath.split(".")
+            prefix, ext = (
+                "_".join(filename_list[:-1]).replace(" ", "_"),
+                filename_list[-1],
+            )
             # 图片不处理
-            if ext == 'jpg':
+            if ext == "jpg":
                 print(f"jump {filepath}")
                 continue
 
             _filepath = f"{prefix}.{ext}"
             os.rename(filepath, _filepath)
 
-            # 把不是mp4的文件转换成mp4，这一步之后不要执行concat
-            mp4_filepath = f"{prefix}.mp4"
-            if ext.lower() != 'mp4' and not os.path.exists(mp4_filepath):
-                print(f'Changing {_filepath} to mp4')
-                result = subprocess.run(
-                    ['ffmpeg', '-i', _filepath, '-vcodec',
-                        'libx264', '-acodec', 'aac', mp4_filepath],
-                    stdout=subprocess.PIPE,
-                    stderr=subprocess.PIPE
-                )
+            _filepath = convert_format(prefix, ext)
 
-                if result.returncode == 0:
-                    print(f"Successfully converted {_filepath} to MP4.")
-                    os.remove(_filepath)
-                else:
-                    print(f"Error converting {_filepath}: {
-                          result.stderr.decode()}")
-                _filepath = mp4_filepath
-
-            thumb_filepath = f"{prefix}.jpg"
-            if not os.path.exists(thumb_filepath):
-                vid_gen_thumb(_filepath)
-
-
+            vid_gen_thumb(_filepath)
 
 
 if __name__ == "__main__":
     # 读取目录
     parser = argparse.ArgumentParser(
         description="跨平台目录处理工具",
-        formatter_class=argparse.ArgumentDefaultsHelpFormatter
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
     )
     parser.add_argument(
-        'path',
-        type=str,
-        help="要处理的目录路径（Windows/Linux均支持）"
+        "path", type=str, help="要处理的目录路径（Windows/Linux均支持）"
     )
     args = parser.parse_args()
     target_dir = os.path.abspath(os.path.normpath(args.path))
